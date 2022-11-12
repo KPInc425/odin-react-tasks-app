bb7552e7533479988eefbd921fa739ccc280c9cd
KP <kpinc425@gmail.com>
2022-11-11 20:39:00 -0800

    Implement ability to edit tasks

--- src/App.js
+++ src/App.js
@@ -12,12 +12,15 @@ class App extends Component {
       taskInput: {
         text: "", 
         id: uniqid(),
+        editState: false,
       },
+      editInput: "",
       tasksTotal: 0,
     };
 
-    this.addTask = this.addTask.bind(this);
-    this.deleteClickedTask = this.deleteClickedTask.bind(this);
+    // this.addTask = this.addTask.bind(this);
+    // this.deleteClickedTask = this.deleteClickedTask.bind(this);
+    // this.makeTaskEditable = this.makeTaskEditable.bind(this);
   }
 
 
@@ -29,6 +32,7 @@ class App extends Component {
       taskInput: {
         text: "",
         id: uniqid(),
+        editState: false,
       },
       tasksTotal: this.state.tasksTotal + 1,
     })
@@ -39,16 +43,85 @@ class App extends Component {
       taskInput: { 
         text: e.target.value,
         id: this.state.taskInput.id,
+        editState: this.state.taskInput.editState
       },
     })
   }
 
+  handleEditChange = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      editInput: e.target.value
+    })
+  }
+
   deleteClickedTask = (e) => {
-    console.log(e.target.parentNode.getAttribute("data-task-id"));
+    // console.log(e.target.parentNode.getAttribute("data-task-id"));
     const clickedTaskID = e.target.parentNode.getAttribute("data-task-id");
     this.setState({
       taskArray: this.state.taskArray.filter((task) => task.id !== clickedTaskID),
       tasksTotal: this.state.tasksTotal - 1,
+    })
+  }
+
+  makeTaskEditable = (e) => {
+    // console.log(e.target.parentNode.getAttribute("data-task-id"));
+    const clickedTaskID = e.target.parentNode.getAttribute("data-task-id");
+    
+    
+    let tmpArray = [].concat(this.state.taskArray);
+    console.log(tmpArray);
+    // Reset editState for other tasks that may be selected
+    tmpArray = this.resetEditStates(tmpArray);
+    console.log(tmpArray);
+    const oldTask = this.findTask(clickedTaskID);
+    const taskIndex = this.findIndex(clickedTaskID);
+    const editableTask = {...oldTask, editState: !oldTask.editState};
+    tmpArray[taskIndex] = editableTask;
+
+
+    this.setState({
+      taskArray: tmpArray,
+    })
+  }
+
+  resetEditStates = (tmpArray) => {
+    return tmpArray.map((task) => {
+      console.log(task);
+      return {
+        ...task,
+        editState: false
+      }
+    })
+  }
+
+  findIndex = (id) => {
+    return this.state.taskArray.findIndex((task) => task.id === id);
+  }
+
+  findTask = (id) => {
+    return this.state.taskArray.find(task => task.id === id);
+  }
+
+  addEditedTask = (e) => {
+    console.log(e.target);
+
+    const editedTaskId = e.target.parentNode.getAttribute("data-task-id");
+    let tmpArray = [].concat(this.state.taskArray);
+
+
+
+    const taskToEdit = this.findTask(editedTaskId);
+    const taskIndex = this.findIndex(editedTaskId);
+
+    const editedTask = {...taskToEdit, text: this.state.editInput};
+
+    tmpArray[taskIndex] = editedTask;
+    tmpArray = this.resetEditStates(tmpArray);
+
+    this.setState({
+      taskArray: tmpArray,
+      editInput: "",
     })
   }
 
@@ -59,7 +132,7 @@ class App extends Component {
           <input type="text" id='taskInput' onChange={ this.handleNameChange } value={ this.state.taskInput.text } placeholder="Enter task here..."></input>
           <button type='submit' style={{ border: "none", background: "none", padding: 0, zIndex: 2}} ><i className="fa-solid fa-square-plus fa-2x" /></button>
         </form>
-        <Overview tasks={ this.state.taskArray } deleteTask={ this.deleteClickedTask }/>
+        <Overview tasks={ this.state.taskArray } deleteTask={ this.deleteClickedTask } setTaskEdit={ this.makeTaskEditable } addEditedTask={ this.addEditedTask } handleEditChange={ this.handleEditChange }/>
         <h2>Total Tasks</h2>
         <p>{ this.state.tasksTotal }</p>
       </div>
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,14 +1,37 @@
 import React from "react";
 
-function Overview({ tasks, deleteTask }) {
+function Overview({ tasks, deleteTask, setTaskEdit, addEditedTask, handleEditChange}) {
     return (
         <div>
             { 
             tasks.map((task, index) => {
                 return (
                     <div key={ task.id } data-task-id={ task.id }>
-                        <p style={{ display: "inline-block", paddingRight: 10}}> { index + 1} { task.text } </p>
-                        <button key={ index }  onClick={ deleteTask } style={{ border: "none", background: "none", padding: 0, zIndex: 2}} className="fa-solid fa-trash-can"> </button>
+                        { task.editState 
+                            ? 
+                            <div style={{ display: "inline-block"}}>
+                                <label>{ index + 1 }) </label>
+                                <input id={ `task${index}` } placeholder="Edit text here..." onChange={ handleEditChange }></input>
+                                <p style={{ display: "inline-block", paddingRight: 10}}> (Current: { task.text }) </p>
+
+                            </div> 
+                            : 
+                            <p style={{ display: "inline-block", paddingRight: 10}}> { index + 1}) { task.text } </p>
+                        }
+                        { task.editState 
+                            ? 
+                            <button onClick={ addEditedTask } style={{ border: "none", background: "none", paddingLeft: 5, zIndex: 2}} className="fa-solid fa-square-plus"> </button> 
+                            :
+                            <button onClick={ setTaskEdit } style={{ border: "none", background: "none", paddingLeft: 5, zIndex: 2}} className="fa-solid fa-pen-to-square"> </button>
+                        }
+                        { task.editState 
+                            ? 
+                            <button onClick={ setTaskEdit } style={{ border: "none", background: "none", padding: 0, zIndex: 2}} className="fa-regular fa-rectangle-xmark"> </button>
+                            :
+                            <button onClick={ deleteTask } style={{ border: "none", background: "none", padding: 0, zIndex: 2}} className="fa-solid fa-trash-can"> </button>
+                        }
+
+                        
                     </div>
                 )
             })
@@ -16,5 +39,7 @@ function Overview({ tasks, deleteTask }) {
         </div>
     )
 }
+
+// function task
 
 export default Overview;
